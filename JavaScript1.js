559e34859a489ee25c7e38849d9378cfa943c0f2
Rin-O-Hatake <gizatullin-damir@bk.ru>
2021-09-01 02:37:28 +0300

    Test Calculator

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,4 +1,20 @@
 'use strict';
-let Clone = { Name: 20 };
-let Clone2 = Object.assign({},Clone);
-alert(Clone.Name);
+
+let calculator = {
+    read()
+    {
+        this.Number1 = +prompt("Значение 1 слогаемого","");
+        this.Number2 = +prompt("Значение 2 слогаемого",""); 
+    },
+    sum()
+    {
+        return this.Number1 + this.Number2;
+    },
+    mul()
+    {
+        return this.Number1 * this.Number2;
+    }
+};
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
